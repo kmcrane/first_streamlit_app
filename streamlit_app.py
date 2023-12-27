642897d7ce323ae35ae8d1fe3fa50f7f70e60583
kmcrane <37196647+kmcrane@users.noreply.github.com>
2023-12-27 09:36:57 -0500

    Update streamlit_app.py
    menu change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents Healthy New Diner!')
+streamlit.title('My Parents Healthy New Diner')
+
 streamlit.header('Breakfast Menu')
-streamlit.text('Oatmeal')
-streamlit.text('Kale and Blueberry Smoothie')
-streamlit.text('Hard Boiled Free Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
